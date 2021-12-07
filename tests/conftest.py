0374133f281c3c7f61dee0c71359e7bd75a53dc9
Microsoft Open Source <microsoftopensource@users.noreply.github.com>
2021-12-06 21:33:03 -0800

    .github/CODE_OF_CONDUCT.md committed
    .github/ISSUE_TEMPLATE.md committed
    
    .github/PULL_REQUEST_TEMPLATE.md committed
    
    Initial check in
    
    Added logging statements
    
    Reorg repo so source code is in root folder.  Scripts will now be in a separate repo
    
    Added title.  Changed Text to be blue
    
    Update readme for article deployment
    
    Add favicon.
    
    Fix imports
    
    Removed datetime import, added os import and formatted flask imports.
    
    Fix readme typo
    
    For container testing.
    
    Main (#1)
    
    * Update README.md
    
    * Add gunicorn
    
    ---------
    
    Co-authored-by: vmagelo <51368918+vmagelo@users.noreply.github.com>
    
    Add or update the Azure App Service build and deployment workflow config
    
    Main (#2)
    
    * Update README.md
    
    * Add gunicorn
    
    * minor fixes
    
    ---------
    
    Co-authored-by: vmagelo <51368918+vmagelo@users.noreply.github.com>
    Co-authored-by: Dmytro Zhovtobriukh <dmytro.zhovtobryukh@hneu.net>
    
    Update README.md
    
    Add gunicorn
    
    minor fixes
    
    minor changes
    
    update config
    
    minor fix
    
    fix
    
    fix error
    
    fix deploy
    
    fix env
    
    change push
    
    remove extra data
    
    new config
    
    fix typo
    
    fix another typo
    
    final fix
    
    minor fix
    
    fix requirements
    
    change env
    
    add some features to CI
    
    add pylint to CI
    
    set pylint score
    
    code update
    
    code fix
    
    fix pylint
    
    add versions
    
    minor refactoring
    
    add workflow
    
    add test
    
    exclude tests from pylint
    
    fix workflow
    
    squash commits

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from my_project import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_request_example(client):
+    response = client.get("/posts")
+    assert b"<h2>Hello, World!</h2>" in response.data
