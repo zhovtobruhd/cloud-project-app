bf74dd35d77778236be18677aa3603cc076f28fb
Dmytro Zhovtobriukh <zhovtobruhd@gmail.com>
2023-12-21 00:40:47 +0200

    add db

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-"""docstring"""
 import os
+import json
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
+
+
+url = os.environ["ACCOUNT_URI"]
+key = os.environ["ACCOUNT_KEY"]
+database_name = os.environ["DATABASE_NAME"]
+container_name = os.environ["CONTAINER_NAME"]
+
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def index():
+    client = CosmosClient(url, key)
+    database = client.get_database_client(database=database_name)
+    container = database.get_container_client(container_name)
+
+
+    data = [json.dumps(item, indent=True) for item in container.query_items(
+        query=f'SELECT * FROM {container_name} c LIMIT 100',
+        enable_cross_partition_query=True
+    )]
     print('Request for index page received')
-    return render_template('index.html')
+    return render_template('index.html', value=str(data))
 
 
 @app.route('/favicon.ico')
@@ -33,3 +51,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+
