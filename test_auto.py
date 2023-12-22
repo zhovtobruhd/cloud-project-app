54e809f069b32961cd24254ca760b0f9d675142f
Dmytro Zhovtobriukh <zhovtobruhd@gmail.com>
2023-12-22 18:41:41 +0200

    fix pylint

--- test_auto.py
+++ test_auto.py
@@ -1,3 +1,4 @@
+"""automated test for app"""
 import os
 import sys
 import asyncio
@@ -24,7 +25,8 @@ print(sys.version)
 # Define connection string
 connectionString = "HostName=yetanotheriothub.azure-devices.net;DeviceId=test;SharedAccessKey=ojuvSLucYlHq2ElvFr1bZuPGtLyJz5/uxAIoTNBt2qA=" # os.environ["DEVICE_CONNECTION_STRING"]
 
-async def sendToIotHub(data):
+async def send_to_iot_hub(data):
+    """sends data to iot hub"""
     try:
         # Create an instance of the IoT Hub Client class
         device_client = IoTHubDeviceClient.create_from_connection_string(connectionString)
@@ -38,13 +40,12 @@ async def sendToIotHub(data):
 
         # Shutdown the client
         await device_client.shutdown()
-        
-    
-    except Exception as e:
-        logging.error("Error:", str(e))
+
+    except Exception as e: # pylint: disable=broad-except
+        print(e)
 
 def main():
-    # Run an infinite while loop to send data every 5 seconds
+    """entry point"""
     # Generate random value
     temperature = random.randint(20, 50)
     # Generate data packet
@@ -53,28 +54,28 @@ def main():
         "temperature":temperature,
         "edge_time_stamp":str(datetime.datetime.now())
     }
-    asyncio.run(sendToIotHub(data=json.dumps(data)))
+    asyncio.run(send_to_iot_hub(data=json.dumps(data)))
     time.sleep(5)
 
     client = CosmosClient(url, {'masterKey': key}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
     try:
         db = client.get_database_client(database_name)
-        logging.debug('Database with id \'{0}\' was found'.format(database_name))
+        print(f'Database with id \'{database_name}\' was found')
 
         container = db.get_container_client(container_name)
-        logging.debug('Container with id \'{0}\' was found'.format(container_name))
+        print(f'Container with id \'{container_name}\' was found')
 
-        d = [item for item in container.query_items(
+        d = list(container.query_items(
                 query=f'SELECT * FROM {container_name} c ORDER BY c._ts DESC OFFSET 0 LIMIT 1',
                 enable_cross_partition_query=True
-            )]
-        
+            ))
+
         msg = d[-1]['payload']
         print("Message received from Cosmos DB: %s", msg)
 
-    except Exception as e:
-        logging.error('func has caught an error. {0}'.format(e.message))
-    
+    except Exception as e:  # pylint: disable=broad-except
+        print(e)
+
     assert data['device_id'] == msg['device_id']
     assert data['temperature'] == msg['temperature']
     assert data['edge_time_stamp'] == msg['edge_time_stamp']
